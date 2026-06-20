eea0acbc9105eb575534aca2f9d60189c4199ea9
aramaki112-wq <aramaki112@gmail.com>
2026-06-21 08:54:17 +0900

    script.js を更新

--- day2-name-input/script.js
+++ day2-name-input/script.js
@@ -3,7 +3,7 @@ const button = document.getElementById("showButton");
 button.addEventListener("click", function () {
 
     const name =
-
+//.valueは入力欄の中身を取得するという意味
     document.getElementById("nameInput").value;
 
     document.getElementById("message").textContent =
